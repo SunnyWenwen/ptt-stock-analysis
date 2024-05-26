ab50108556884c857ff2cd05c49c4ef1d882ee93
Sunny <p147896325p@gmail.com>
2024-05-26 18:16:32 +0800

    Set silent  mode for get_stock_code_and_date_return()

--- utils.py
+++ utils.py
@@ -18,16 +18,19 @@ def get_stock_code_and_date_return(stock_code_and_date_df, code_col_name, date_c
     return_dict_list = []
     for itr, row in stock_code_and_date_df.iterrows():
         # 每100筆資料印一次
+        dash = '-' * 10
         if itr % 50 == 0:
-            print(f'-------------------------Already processed {itr} articles.-------------------------')
+            print(f'{dash}Already processed {itr}/ {len(stock_code_and_date_df)} articles.{dash}')
         # row = see.loc[12]
         tmp_return_dict = {}
         tmp_return_dict[code_col_name] = row[code_col_name]
         tmp_return_dict[date_col_name] = row[date_col_name]
         try:
             tmp_return_dict.update(
-                MyStock(row[code_col_name], initial_fetch=False).cal_return(pd.to_datetime(row[date_col_name]),
-                                                                            test_day_list=return_days_int_list))
+                MyStock(row[code_col_name], initial_fetch=False, silent=True).cal_return(
+                    pd.to_datetime(row[date_col_name]),
+                    test_day_list=return_days_int_list,
+                    silent=True))
             tmp_return_dict['is_success_get_return_data'] = True
         except:
             tmp_return_dict['is_success_get_return_data'] = False
