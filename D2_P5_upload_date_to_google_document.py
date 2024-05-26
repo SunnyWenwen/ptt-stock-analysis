f68362e0179a3d4fadd78eecedb9aa778bde65c1
Sunny <p147896325p@gmail.com>
2024-05-26 20:20:51 +0800

    Add new feature: Upload data(all_target_article_return & author_return_summary) to google document

---
+++ D2_P5_upload_date_to_google_document.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import pygsheets
+
+from config import xlsx_path
+
+gc = pygsheets.authorize(service_file='upload_date_to_google_document.json')
+sht = gc.open_by_url(
+    'https://docs.google.com/spreadsheets/d/1vNf7zi-a1KM_c2CsLQL_HmO6rdsfwmPHnjVC-pAEP6k/edit#gid=0'
+)
+# 列出所有
+wks_list = sht.worksheets()
+print(wks_list)
+
+# 讀取需要寫入的檔案
+all_target_article_return_df = pd.read_excel(xlsx_path + 'all_target_article_return.xlsx')
+author_return_summary = pd.read_excel(xlsx_path + 'author_return_summary.xlsx')
+
+# 開始寫入
+
+print('Start upload to google document.')
+print('Start upload all_target_article_return.')
+# 先寫入all_target_article_return
+wks = sht.worksheet_by_title('all_target_article_return')
+# 可能會超過上限，最新的排前面
+all_target_article_return_df.sort_values(by=['post_date'], inplace=True, ignore_index=True, ascending=False)
+
+# error應該是googleapiclient.errors.HttpError
+try:
+    wks.set_dataframe(all_target_article_return_df, 'A1')  # 從欄位 A1 開始
+except Exception as e:
+    print(e)
+print('all_target_article_return has been uploaded.')
+
+print('Start upload author_return_summary.')
+# 寫入author_return_summary
+wks = sht.worksheet_by_title('author_return_summary')
+wks.set_dataframe(author_return_summary, 'A1')  # 從欄位 A1 開始
+print('author_return_summary has been uploaded.')
+
+print('Complete upload to google document.')
+
+# # 測試df
+# df = pd.DataFrame({
+#     'Name': ['Alice', 'Bob', 'Charlie'],
+#     'Age': [25, 30, 35],
+#     'City': ['New York', 'Los Angeles', 'Chicago']
+# })
+# wks.set_dataframe(df, 'A1')  # 從欄位 A1 開始
